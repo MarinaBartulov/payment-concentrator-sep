b8ad8f8dc7068550f7a6fce3541ec0bbc258a9dc
KliskaB <52880798+KliskaB@users.noreply.github.com>
2020-12-13 00:15:30 +0100

    Add PCC service app, Issuer Bank app and Bank Payment Service Frontend app

---
+++ issuer-bank-service/src/main/java/team16/issuerbankservice/IssuerBankServiceApplication.java
@@ -0,0 +1,13 @@
+package team16.issuerbankservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssuerBankServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssuerBankServiceApplication.class, args);
+	}
+
+}
