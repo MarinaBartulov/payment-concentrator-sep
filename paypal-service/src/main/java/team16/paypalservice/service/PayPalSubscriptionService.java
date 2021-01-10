ca0916131f270d388ec75ee3a81866482d414d64
Senka <senka.soic@uns.ac.rs>
2021-01-10 17:31:37 +0100

    Added scheduled finding unfinished transactions and subscriptions.

--- paypal-service/src/main/java/team16/paypalservice/service/PayPalSubscriptionService.java
+++ paypal-service/src/main/java/team16/paypalservice/service/PayPalSubscriptionService.java
@@ -2,8 +2,12 @@ package team16.paypalservice.service;
 
 import team16.paypalservice.model.PayPalSubscription;
 
+import java.util.List;
+
 public interface PayPalSubscriptionService {
 
     PayPalSubscription save(PayPalSubscription subscription);
     PayPalSubscription getOne(Long id);
+    void findUnfinishedSubscriptions();
+    List<PayPalSubscription> findAllUnfinished();
 }
